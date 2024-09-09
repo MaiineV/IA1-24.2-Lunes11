e4b0b0ad25d43095a814f1574eececa09df686b7
MaiineV <agustinmartinezbarrios2@hotmail.com>
2024-09-09 12:32:52 -0300

    Boids

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Boid : MonoBehaviour
@@ -17,6 +18,12 @@ public class Boid : MonoBehaviour
     [Range(0,1)]public float fleeWeight;
     [Range(0,1)]public float seekWeight;
 
+    [Range(0,1)]public float separationWeight;
+    [Range(0,1)]public float cohesionWeight;
+    [Range(0,1)]public float allignWeight;
+
+    GameObject[] closeBoids;
+
     void Awake()
     {
         var z = Random.Range(-1f, 1f);
@@ -27,10 +34,21 @@ public class Boid : MonoBehaviour
 
     void Update()
     {
-        Seek();
-        Flee();
-        transform.forward = fleeVector * fleeWeight + seekVector * seekWeight;
+        closeBoids = Physics.OverlapSphere(transform.position, 5)
+            .Select(x => x.gameObject)
+            .ToArray();
+
+        //Seek();
+        //Flee();
+        //transform.forward = fleeVector * fleeWeight + seekVector * seekWeight;
+        //transform.position += transform.forward * speed * Time.deltaTime;
+
+        transform.forward = Separation().normalized * separationWeight
+            + Cohesion().normalized * cohesionWeight
+            + Allign().normalized * allignWeight;
+
         transform.position += transform.forward * speed * Time.deltaTime;
+        BoidManager.Instance.CheckBounds(this);
     }
 
     public void Seek()
@@ -45,5 +63,45 @@ public class Boid : MonoBehaviour
         var dir = transform.position - danger.position;
         dir.y = 0;
         fleeVector = transform.forward * (1 - rotationSpeed) + dir * rotationSpeed;
+    }
+
+    public Vector3 Cohesion()
+    {
+        var dir = Vector3.zero;
+
+        foreach (var t in closeBoids)
+        {
+            dir += t.transform.position - transform.position;
+        }
+
+        dir.y = 0;
+        return dir;
+    }
+
+    public Vector3 Allign()
+    {
+        var dir = Vector3.zero;
+
+        foreach (var t in closeBoids)
+        {
+            dir += t.transform.forward;
+        }
+        dir.y = 0;
+
+        return dir;
+    }
+
+    public Vector3 Separation()
+    {
+        var dir = Vector3.zero;
+
+        foreach (var t in closeBoids)
+        {
+            dir +=  transform.position- t.transform.position;
+        }
+
+        dir.y = 0;
+
+        return dir;
     }
 }
